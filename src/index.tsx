b1c3313c1a5cbcad3b61a932952ba7004f63ab22
bbbb <bgh9651@gmail.com>
2022-10-25 17:57:30 +0900

    main, styled-components 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import theme from './styles/theme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global-styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
