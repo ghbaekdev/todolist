70b288dece9a0a75f2ec69fb7e1eec968faa9f30
bbbb <bgh9651@gmail.com>
2022-10-28 17:03:10 +0900

    feature/main news api 오늘날짜 받아오게 axios모듈 수정

--- src/lib/api/newsApi.ts
+++ src/lib/api/newsApi.ts
@@ -1,6 +1,12 @@
 import { instance } from './api';
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+
+const today = `${year}-${month}-${day}`;
 
 export const getNewsList = async () => {
-  const response = await instance.get('/news');
+  const response = await instance.get(`/news?writedAt=${today}`);
   return response.data;
 };
