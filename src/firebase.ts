4d31f8eb64eff7f0b9ff63054e54b073e58568b2
bbbb <bgh9651@gmail.com>
2022-12-02 21:14:37 +0900

    s3 test5

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase produce ts that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: 'AIzaSyCkRCg0X8qfSXmE4ROLCi-7Yx7sS47p3Nw',
   authDomain: 'baeks-todo.firebaseapp.com',
