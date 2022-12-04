fb4531dc03595fa498b50b1b1006fa5b0ae70e4f
bbbb <bgh9651@gmail.com>
2022-12-04 16:20:38 +0900

    refactor(todoform) 요일 순서 로직 수정

--- src/pages/Main/components/TodoList/TodoList.tsx
+++ src/pages/Main/components/TodoList/TodoList.tsx
@@ -5,6 +5,8 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import * as api from '../../../../lib/api/todoApi';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import { WEEKDAYDATA } from '../../../../lib/data/WEEKDAY';
+import { selectedDaysElement } from '../../../../types/TodoType';
 
 const TodoList = () => {
   const navigate = useNavigate();
@@ -50,10 +52,16 @@ const TodoList = () => {
             let selectedDays;
             if (item.days) {
               const daysArray = Object.entries(item.days);
-              selectedDays = daysArray.filter(([day, selected]) => {
+              const filterArray = daysArray.filter(([day, selected]) => {
                 return selected && day;
               });
+              selectedDays = filterArray.sort(
+                (a: selectedDaysElement, b: selectedDaysElement) => {
+                  return WEEKDAYDATA[a[0]] - WEEKDAYDATA[b[0]];
+                }
+              );
             }
+
             let checked;
             if (localStorage.getItem(`${key}`)) {
               checked = JSON.parse(localStorage.getItem(`${key}`)!);
