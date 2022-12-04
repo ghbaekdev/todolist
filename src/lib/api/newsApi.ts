86e729852c0577f8a04e5649e8860e449e8fba03
bbbb <bgh9651@gmail.com>
2022-12-04 20:31:21 +0900

    test

--- src/lib/api/newsApi.ts
+++ src/lib/api/newsApi.ts
@@ -1,3 +1,23 @@
+// import { NewsType } from '../../types/NewsType';
+// import { instance } from './api';
+
+// let date = new Date();
+// let year = date.getFullYear();
+// let month = date.getMonth() + 1;
+// let day = date.getDate();
+
+// const today = `${year}-${month}-${day}`;
+
+// export const getNewsList = async () => {
+//   const response = await instance.get(`/news.json`);
+
+//   return response.data.filter((news: NewsType) => {
+//     if (month < 10) return news.writedAt === `${year}-0${month}-${day}`;
+//     if (day < 10) return news.writedAt === `${year}-${month}-0${day}`;
+//     return news.writedAt === today;
+//   });
+// };
+
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
