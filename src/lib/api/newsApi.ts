a35495478d4b8ac79d939c078928c2fe71d28c6e
bbbb <bgh9651@gmail.com>
2022-12-04 19:12:32 +0900

    fixed to newsapi

--- src/lib/api/newsApi.ts
+++ src/lib/api/newsApi.ts
@@ -4,11 +4,12 @@ const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
 export const instance = axios.create({
   baseURL: 'https://newsapi.org/v2',
+  headers: {
+    'X-Api-Key': `${API_KEY}`,
+  },
 });
 
 export const getNewsList = async () => {
-  const response = await instance.get(
-    `/top-headlines?country=kr&apiKey=${API_KEY}`
-  );
+  const response = await instance.get(`/top-headlines?country=kr`);
   return response.data;
 };
